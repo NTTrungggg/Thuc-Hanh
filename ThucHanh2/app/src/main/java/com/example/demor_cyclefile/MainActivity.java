b6869c984f329cab4f718fc325844e530a80feac
NTTrunggg <2251172534@e.tlu.edu.vn>
2025-07-30 22:19:40 +0700

    Upload thuc hanh 2

--- ThucHanh2/app/src/main/java/com/example/demor_cyclefile/MainActivity.java
+++ ThucHanh2/app/src/main/java/com/example/demor_cyclefile/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.demor_cyclefile;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btncall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall); // Ensure R.id.btncall exists in activity_main.xml
+
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                // Ensure 'subactivity.class' exists and is in the same package, or import its package.
+                Intent intent1 = new Intent(MainActivity.this, Subactivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
