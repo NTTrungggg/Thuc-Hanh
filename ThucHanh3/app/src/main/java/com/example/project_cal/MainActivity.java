5d31d72bc122f74d438d9b0935fcd93a86ace5eb
NTTrunggg <2251172534@e.tlu.edu.vn>
2025-07-30 22:31:29 +0700

    Upload thuc hanh 3

--- ThucHanh3/app/src/main/java/com/example/project_cal/MainActivity.java
+++ ThucHanh3/app/src/main/java/com/example/project_cal/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.project_cal;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
