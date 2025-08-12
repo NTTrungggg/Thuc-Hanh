3fe4a930c9f9ca654bb773fe3dbc38cb4f2b96bb
NTTrunggg <2251172534@e.tlu.edu.vn>
2025-08-12 09:02:27 +0700

    ThucHanh7.2 upload

--- ThucHanh7.2/app/src/main/java/com/example/thuchanh72/MainActivity.java
+++ ThucHanh7.2/app/src/main/java/com/example/thuchanh72/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.thuchanh72;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtA, txtB;
+    Button btnKetQua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtA = findViewById(R.id.txta);
+        txtB = findViewById(R.id.txtb);
+        btnKetQua = findViewById(R.id.btnketqua);
+
+        btnKetQua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(txtA.getText().toString());
+                double b = Double.parseDouble(txtB.getText().toString());
+
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("hsa", a);
+                intent.putExtra("hsb", b);
+                startActivity(intent);
+            }
         });
     }
 }
